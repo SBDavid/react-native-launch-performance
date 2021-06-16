da5ad705f9b86059c0262918afec254f711587fa
jiawei.tang <billl89@126.com>
2021-06-16 15:30:17 +0800

    feat: 测试，接受到content_appeared后，可以获取到模块加载时长

--- example2/index.js
+++ example2/index.js
@@ -3,8 +3,23 @@
  */
 
 import LaunchPerformance from '@xmly/react-native-launch-performance';
-import { addPlugin } from 'react-native-flipper';
-LaunchPerformance.loadPlugin(addPlugin);
+// import { addPlugin } from 'react-native-flipper';
+// LaunchPerformance.loadPlugin(addPlugin);
+
+const observer = new LaunchPerformance.PerformanceObserver(LaunchPerformance.performance, (entry) => {
+  console.info(entry.name, entry.detail, entry.duration, entry.startTime);
+  if (entry.name.indexOf("contentAppeared") !== -1) {
+    const measures = LaunchPerformance.markListener.getJsModuleMeasure();
+    console.info(measures[0]);
+  }
+});
+
+observer.observe({
+  types: ['mark']
+});
+
+LaunchPerformance.markListener.listenForReactNativeMarker();
+LaunchPerformance.markListener.getMeasure();
 
 import {AppRegistry} from 'react-native';
 import App from './App';
